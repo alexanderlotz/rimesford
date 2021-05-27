f4bfe4f2e96d9ae5167eb1382194f3465bae193d
Madeleine Street <kazul8@me.com>
2021-05-26 23:43:33 -0400

    Feat: add village map, controllable character and pixel art assets

---
+++ Rimesford/Scripts/PixPlayer.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+export var speed = 200
+export var feet = Vector2()
+
+var face_front = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	feet.x = $Feet.position.x
+	feet.y = $Feet.position.y
+	print(feet)
+
+# Returns a vector describing the current velocity of the character based
+# on input received
+func get_input():
+	var velocity = Vector2()
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+	return velocity
+
+
+func _physics_process(delta):
+	var velocity = get_input()
+	
+	# Pick animaiton
+	if velocity.y > 0:
+		$Area2D/AnimatedSprite.animation = "walk_front"
+		face_front = true
+	if velocity.y < 0:
+		$Area2D/AnimatedSprite.animation = "walk_back"
+		face_front = false
+	if velocity.y == 0 and face_front:
+		$Area2D/AnimatedSprite.animation = "idle_front"
+	if velocity.y == 0 and not face_front:
+		$Area2D/AnimatedSprite.animation = "idle_back"
+		
+	# TODO: add left and right when animations are finished
+		
+	# Move the character
+	velocity = move_and_slide(velocity)
+
+func get_feet():
+	return feet
